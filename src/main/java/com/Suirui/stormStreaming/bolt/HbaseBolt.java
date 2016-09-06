edaec6a90904a5e76758399b3c5cde37128db4d0
sharon-yuan <wangshouyuan@aliyun.com>
2016-09-06 17:28:57 +0800

    build

--- src/main/java/com/Suirui/stormStreaming/bolt/HbaseBolt.java
+++ src/main/java/com/Suirui/stormStreaming/bolt/HbaseBolt.java
@@ -1,23 +1,21 @@
 package com.Suirui.stormStreaming.bolt;
 
 import java.util.Properties;
-
 import org.apache.hadoop.hbase.client.HConnection;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.log4j.Logger;
-
 import backtype.storm.task.OutputCollector;
 
 public class HbaseBolt {
 	private static final Logger LOG = Logger.getLogger(HbaseBolt.class);
 	
-	private static final String RP_TABLE_NAME="rp_msg";
+	private static final String RP_TABLE_NAME="rp";
 	private static final String RP_TABLE_COLUMN_FAMILY_NAME="msg";
 	
-	private static final String HUIJIAN_TABLE_NAME="huijian_msg";
+	private static final String HUIJIAN_TABLE_NAME="huijian";
 	private static final String HUIJIAN_TABLE_COLUMN_FAMILY_NAME="msg";
 	
-	private static final String ZOOM_TABLE_NAME="zoom_msg";
+	private static final String ZOOM_TABLE_NAME="zoom";
 	private static final String ZOOM_TABLE_COLUMN_FAMILY_NAME="msg";
 	
 	private OutputCollector collector;
